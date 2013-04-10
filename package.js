e79f007892a83e8f8cb19fa2fdf786b47bc7a573
Fritz Gerneth <fgerneth@effms.de>
2013-04-10 02:01:21 +0200

    Clean up package.js

--- package.js
+++ package.js
@@ -1,20 +1,16 @@
 Package.describe({
-  summary: "A reactive version of window.location"
+    summary: "A reactive version of window.location with history support"
 });
 
 Package.on_use(function (api, where) {
-  api.use('deps', 'client');
-  api.use('page-js-ie-support', 'client');
-  api.use('underscore', ['client', 'server']);
+  api.use(['deps', 'page-js-ie-support'], 'client');
   
   api.add_files('location.js', 'client');
 });
 
 
 Package.on_test(function (api) {
-  api.use('router', ['client', 'server']);
-  api.use('test-helpers', ['client', 'server']);
-  api.use('tinytest', ['client', 'server']);
+  api.use(['tinytest', 'meteor-location'], 'client');
   
   api.add_files('location_tests.js', 'client');
 });
